7e89e5417e79a7bfd09634eef4739a1ae187bfdf
ntsako <ntsakokhozacc@gmail.com>
2021-05-31 00:46:34 +0200

    initial commit

---
+++ ASPPractice/ASPPractice/Data/ASPPracticeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPPractice.Models;
+
+namespace ASPPractice.Data
+{
+    public class ASPPracticeContext : DbContext
+    {
+        public ASPPracticeContext (DbContextOptions<ASPPracticeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPPractice.Models.joke> joke { get; set; }
+    }
+}
